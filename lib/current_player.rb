eacfb1971b1cf5a987a88f01000a175124400e40
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-09 17:32:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,10 @@
 #turn_count
 def turn_count(board)
+  counter = 0
   board.each do |boards|
-    puts "This is turn number #{boards}"
+    puts "This is turn number #{boards}."
+    counter += 1
+  end
 end
 
 #current_player
