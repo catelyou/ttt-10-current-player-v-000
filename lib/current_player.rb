a5682dc31f7e72a9c946c635783f78ef5cc01df8
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-09 17:36:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,9 +2,11 @@
 def turn_count(board)
   counter = 0
   board.each do |space|
-
-    counter += 1
+    if space == "X" || space == "O"
+      counter += 1
+    end
   end
+return counter
 end
 
 #current_player
